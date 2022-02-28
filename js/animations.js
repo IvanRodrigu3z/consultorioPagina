4936c10b3a559025067ed5e30a4282fe6b8990f4
Ivan Rodriguez <culma6794@gmail.com>
2022-02-28 01:31:32 -0500

    mejoras visiales

--- js/animations.js
+++ js/animations.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function() { //rediraccionar al contenido meciante en menú
     $('a[href^="#"]').click(function() {
       var destino = $(this.hash);
       if (destino.length == 0) {
@@ -10,6 +10,23 @@ $(document).ready(function() {
       $('html, body').animate({ scrollTop: destino.offset().top }, 1000);
       return false;
     });
+});
 
-    
-  });
+
+
+$(window).scroll(function(){ //para mostrar foto de los maestros
+    var scrollActual = $(document).scrollTop();
+    if(scrollActual > 100){
+        $('.img-master').show(1000);
+    }else if(scrollActual < 500){
+        $('.img-master').hide();
+    }
+});
+
+
+$(function(){ // boton ir arriba
+    $('#up').click(function(event){
+        event.preventDefault();
+        $('html, body').animate({scrollTop:0}, '1800');
+    });
+});
