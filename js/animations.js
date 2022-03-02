9d610d4044e16a67d5060eadbc975a019a59f4c8
Ivan Rodriguez <culma6794@gmail.com>
2022-03-01 20:41:37 -0500

    incorporando mejoras

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,6 @@
 $(document).ready(function() { //rediraccionar al contenido meciante en menú
     $('a[href^="#"]').click(function() {
-      var destino = $(this.hash);
+      let destino = $(this.hash);
       if (destino.length == 0) {
         destino = $('a[name="' + this.hash.substr(1) + '"]');
       }
@@ -13,6 +13,19 @@ $(document).ready(function() { //rediraccionar al contenido meciante en menú
 });
 
 
+// animaciones al hacer scroll //
+
+$(window).scroll(function(){ //menu fijo
+    let scrollActual = $(document).scrollTop();
+    let nav = $('.navbar');
+    let showNav = parseInt($('#masters').offset().top);
+    if(scrollActual > showNav){
+        nav.addClass("fixed");
+    }else if(scrollActual < showNav){
+        nav.removeClass("fixed")
+    }
+});
+
 
 $(window).scroll(function(){ //para mostrar foto de los maestros
     var scrollActual = $(document).scrollTop();
@@ -23,10 +36,13 @@ $(window).scroll(function(){ //para mostrar foto de los maestros
     }
 });
 
-
-$(function(){ // boton ir arriba
-    $('#up').click(function(event){
-        event.preventDefault();
-        $('html, body').animate({scrollTop:0}, '1800');
-    });
+$(window).scroll(function(){ //para mostrar servicios
+    var scrollActual = $(document).scrollTop();
+    var scrollService = parseInt($('.ultimo').offset().top);
+    console.log("ultimo", scrollService, "scroll actual", scrollActual);
+    if(scrollActual > scrollService){
+        $('.message').animate({width: '100%'}, 400);
+    } else{
+        $('.message').width('50%');
+    }
 });
